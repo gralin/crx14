6ac53b6161667450b8fa70a5fbcfdbdccf7b72a7
Jakub Bartkowiak <gralinpl@gmail.com>
2012-01-04 12:17:39 +0100

    Initial commit

---
+++ Source/Samples/SimpleProject/Program.cs
@@ -0,0 +1,50 @@
+﻿using System.Threading;
+using Microsoft.SPOT;
+using Microsoft.SPOT.Hardware;
+
+namespace Gralin.NETMF.ST.CRX14
+{
+    public class Program
+    {
+        // Adjusted for FEZ Domino
+        public const Cpu.Pin SdaPin = (Cpu.Pin)33; //FEZ_Pin.Digital.Di2
+        public const Cpu.Pin SclPin = (Cpu.Pin)31; //FEZ_Pin.Digital.Di3
+        public const Cpu.Pin LedPin = (Cpu.Pin)4; //FEZ_Pin.Digital.LED
+
+        // This is my tag uid, ours will be different ;)
+        public const string MyTagUid = "1910D552031A02D0";
+
+        public static void Main()
+        {
+            var crx14 = CRX14.UseSoftwareI2C(0, SdaPin, SclPin);
+            var led = new OutputPort(LedPin, false);
+
+            Debug.Print("Waiting for TAG...");
+
+            while (true)
+            {
+                var tags = crx14.FindTags();
+
+                if (tags != null && tags.Length > 0)
+                {
+                    Debug.Print("Found: " + tags.Length);
+
+                    foreach (var tag in tags)
+                    {
+                        Debug.Print("TAG: " + tag);
+                        var eeprom = crx14.ReadEeprom(tag);
+                        Debug.Print("Eeprom: " + eeprom.Length + " bytes");
+                    }
+
+                    break;
+                }
+
+                Thread.Sleep(1000);
+            }
+
+            led.Write(true);
+            Debug.Print("Finished");
+            Thread.Sleep(Timeout.Infinite);
+        }
+    }
+}
---
+++ Source/SoftwareI2C/SoftwareI2CDevice.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.SPOT.Hardware;
+
+namespace Gralin.NETMF.ST.CRX14.SoftwareI2C
+{
+    public class SoftwareI2CDevice : IDevice
+    {
+        private readonly SoftwareI2C _device;
+        private readonly byte _readCmd;
+        private readonly byte _writeCmd;
+
+        public SoftwareI2CDevice(byte address, Cpu.Pin sdaPin, Cpu.Pin sclPin)
+        {
+            _device = new SoftwareI2C(sclPin, sdaPin, 400);
+            _writeCmd = (byte) ((address << 1) + 0);
+            _readCmd = (byte) ((address << 1) + 1);
+        }
+
+        #region IDevice Members
+
+        public bool Write(byte regAddress, byte[] data)
+        {
+            var noData = data == null || data.Length == 0;
+
+            _device.Transmit(true, false, _writeCmd);
+            _device.Transmit(false, noData, regAddress);
+
+            if (!noData)
+                for (var i = 0; i < data.Length; i++)
+                    _device.Transmit(false, i + 1 == data.Length, data[i]);
+
+            return true;
+        }
+
+        public byte[] Read(byte regAddress, byte dataLength)
+        {
+            return WriteRead(regAddress, null, dataLength);
+        }
+
+        public byte[] WriteRead(byte regAddress, byte[] data, byte readDataLength)
+        {
+            Write(regAddress, data);
+
+            var noData = readDataLength == 0;
+            var result = new byte[readDataLength];
+
+            _device.Transmit(true, noData, _readCmd);
+
+            if (!noData)
+                for (var i = 0; i < readDataLength; i++)
+                    result[i] = _device.Receive(i + 1 < readDataLength, i + 1 == readDataLength);
+
+            return result;
+        }
+
+        #endregion
+    }
+}
